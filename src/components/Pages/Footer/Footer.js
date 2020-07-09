a0612cecc20a5bd6ad905954d5ea8072307236ba
Maxime Bailly <maxime3510@gmail.com>
2020-07-09 19:55:33 +0200

    Add CSS Footer

--- src/components/Pages/Footer/Footer.js
+++ src/components/Pages/Footer/Footer.js
@@ -3,11 +3,11 @@ import React, {Component} from 'react';
 class Footer extends Component {
     render() {
         return (
-            <div style={{backgroundColor: '#073855', color: 'white', textAlign: "center"}}>
-                <p style={{visibility: "hidden"}}>>Espace</p>
-                <p>Copyright © 2020 Lojeris</p>
-                <p style={{visibility: "hidden"}}>>Espace</p>
-            </div>
+            <footer class="site-footer">
+                <div class="container">
+                    <p>Copyright © 2020 Lojeris</p>
+                </div>
+            </footer>
         );
     }
 }
