3c1458273306436ad34ea0ef1a6eac2cf116fe1e
Yannoël <yannoel.martin@hotmail.fr>
2020-07-09 18:07:41 +0200

    maj design navbar

--- src/components/Pages/Nav/Nav.js
+++ src/components/Pages/Nav/Nav.js
@@ -4,35 +4,29 @@ import { Link, NavLink } from 'react-router-dom';
 class Nav extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg" style={{backgroundColor: "#343a40"}}>
+                <p style={{visibility: "hidden"}}>>Espace</p>
                 <div style={{display: "inline-block", width: "10%"}}></div>
                 <div style={{display: "inline-block", width: "20%"}}>
                     <div style={{display: "block"}}>
-                        <p style={{visibility: "hidden"}}>>Espace</p>
-                        <Link className="navbar-brand" to="/">Lojeris</Link>
-                        <p style={{visibility: "hidden"}}>>Espace</p>
+                        <Link className="navbar-brand" to="/" style={{color: "white", textDecoration: "none"}}>Lojeris</Link>
                     </div>
                 </div>
                 <div style={{display: "inline-block", width: "60%"}} className="collapse navbar-collapse" id="navbarSupportedContent">
                     <div className="navbar-nav mr-auto" style={{display: "flex", justifyContent: "space-between"}}>
                         <div className="nav-item">
-                            <p style={{visibility: "hidden"}}>>Espace</p>
-                            <NavLink to="/" exact className="nav-link">Home</NavLink>
-                            <p style={{visibility: "hidden"}}>>Espace</p>
+                            <NavLink to="/" exact className="nav-link" style={{color: "white", textDecoration: "none"}}>Home</NavLink>
                         </div>
                         <div className="nav-item">
-                            <p style={{visibility: "hidden"}}>>Espace</p>
-                            <NavLink to="/list" exact className="nav-link">List</NavLink>
-                            <p style={{visibility: "hidden"}}>>Espace</p>
+                            <NavLink to="/list" exact className="nav-link" style={{color: "white", textDecoration: "none"}}>List</NavLink>
                         </div>
                         <div className="nav-item">
-                            <p style={{visibility: "hidden"}}>>Espace</p>
-                            <NavLink to="/post" exact className="nav-link">Post</NavLink>
-                            <p style={{visibility: "hidden"}}>>Espace</p>
+                            <NavLink to="/post" exact className="nav-link" style={{color: "white", textDecoration: "none"}}>Post</NavLink>
                         </div>
                     </div>
                 </div>
                 <div style={{display: "inline-block", width: "10%"}}></div>
+                <p style={{visibility: "hidden"}}>>Espace</p>
             </nav>
         );
     }
