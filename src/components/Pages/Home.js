ec459dbbe514f986ff1dc21528b3deb50e114f16
Yannoël <yannoel.martin@hotmail.fr>
2020-07-09 16:01:51 +0200

    avancées page & rooter

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,18 +1,15 @@
 import React, {Component} from 'react';
 import FilterBuildingsList from "../FilterBuildingsList/FilterBuildingsList";
-import Footer from "./Footer/Footer";
 
 class Home extends Component {
     render() {
         return (
             <section>
-                <div>Menu</div>
                 <div>En-tête</div>
                 <FilterBuildingsList
                     nbMaxElem={12}
                     page={1}
                 />
-                <Footer />
             </section>
         );
     }
