44025d2c42b2baa049376ae03569197884163124
Maxime Bailly <maxime3510@gmail.com>
2020-07-10 16:00:40 +0200

    Fix console error

--- src/components/Pages/Nav/Nav.js
+++ src/components/Pages/Nav/Nav.js
@@ -4,14 +4,14 @@ import { Link, NavLink } from 'react-router-dom';
 class Nav extends Component {
     render() {
         return (
-            <div class="container main-header">
-                <Link className="navbar-brand" to="/" class="logo"><span class="fa-stack fa-lg">
-                    <i class="fa fa-home fa-stack-1x fa-inverse"></i>
+            <div className="container main-header">
+                <Link className="navbar-brand logo" to="/"><span className="fa-stack fa-lg">
+                    <i className="fa fa-home fa-stack-1x fa-inverse"></i>
                 </span></Link>
-            <nav class="main-nav">
+            <nav className="main-nav">
                 <ul>
                     <li><NavLink to="/" exact className="nav-link">Accueil</NavLink></li>
-                    <li><a href="#">Notre Agence</a></li>
+                    <li><NavLink to="" exact className="nav-link">Notre Agence</NavLink></li>
                     <li><NavLink to="/list" exact className="nav-link">Biens Immobiliers</NavLink></li>
                     <li><NavLink to="/post" exact className="nav-link">Proposez votre bien</NavLink></li>
                 </ul>
